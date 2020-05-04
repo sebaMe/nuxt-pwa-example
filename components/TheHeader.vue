c3de25f735bee292943299f77cd09e6a65ef7ca7
merker.s <merker.s@st-sportservice.com>
2020-05-04 16:04:34 +0200

    changes styles

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,21 +1,19 @@
 <template>
-    <div class="component__header">
-        <img class="logo" src="../assets/images/icon.png">
-        <nuxt-link to="/">
-            <h2 class="title">
-                Nuxt PWA Example
-            </h2>
-        </nuxt-link>
+  <div class="component__header">
+    <img class="logo" src="../assets/images/icon.png" />
+    <nuxt-link to="/" exact>
+      <h2 class="title">Nuxt PWA Example</h2>
+    </nuxt-link>
 
-        <nav>
-            <nuxt-link to="/launches">
-                <h3>Launches</h3>
-            </nuxt-link>
-            <nuxt-link to="/about">
-                <h3>About</h3>
-            </nuxt-link>
-        </nav>
-    </div>
+    <nav>
+      <nuxt-link to="/launches" exact>
+        <h3>Launches</h3>
+      </nuxt-link>
+      <nuxt-link to="/about" exact>
+        <h3>About</h3>
+      </nuxt-link>
+    </nav>
+  </div>
 </template>
 
 <script>
