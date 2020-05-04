59cb7d587e7285cab97de8746d936cdd79100508
merker.s <merker.s@st-sportservice.com>
2020-05-04 14:59:45 +0200

    updated responsibility

--- pages/launches.vue
+++ pages/launches.vue
@@ -1,39 +1,25 @@
 <template>
-    <div class="launches-page content-page">
-        <h1>Upcoming Launches</h1>
-        <ul class="launches-list">
-            <li v-for="launch in launches"
-                :key="launch.flight_number"
-                class="launch-item">
-                <div class="header">
-                    <div class="number">
-                        {{ launch.flight_number }}
-                    </div>
-                    <div class="name">
-                        {{ launch.mission_name }}
-                    </div>
-                </div>
-                <div class="content">
-                    <div class="date">
-                        <div class="label">
-                            Start Date
-                        </div>
-                        <div class="value">
-                            {{ new Date(launch.launch_date_unix * 1000).toDateString() }}
-                        </div>
-                    </div>
-                    <div class="rocket">
-                        <div class="label">
-                            Rocket
-                        </div>
-                        <div class="value">
-                            {{ launch.rocket.rocket_name }}
-                        </div>
-                    </div>
-                </div>
-            </li>
-        </ul>
-    </div>
+  <div class="launches-page content-page">
+    <h2>Upcoming Launches</h2>
+    <ul class="launches-list">
+      <li v-for="launch in launches" :key="launch.flight_number" class="launch-item">
+        <div class="header">
+          <div class="number">{{ launch.flight_number }}</div>
+          <div class="name">{{ launch.mission_name }}</div>
+        </div>
+        <div class="content">
+          <div class="date">
+            <div class="label">Start Date</div>
+            <div class="value">{{ new Date(launch.launch_date_unix * 1000).toDateString() }}</div>
+          </div>
+          <div class="rocket">
+            <div class="label">Rocket</div>
+            <div class="value">{{ launch.rocket.rocket_name }}</div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
